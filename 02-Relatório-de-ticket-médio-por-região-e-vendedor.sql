f13f8853e14301e4feaf41740c10dfcb80c82760
maikjam <maikonlucas3135@outlook.com>
2026-03-25 13:50:07 -0300

    relatório de ticket médio
    - Cálculo de métrica quantitativa (Média de Vendas) cruzando dados de território.
    - Padronização de nomenclatura para fácil manutenção e auditoria de dados.

---
+++ 02-Relatório-de-ticket-médio-por-região-e-vendedor.sql
@@ -0,0 +1,8 @@
+/*
+2.  Relatório de ticket médio por região ou vendedor.
+
+evento venda media fato gerador
+entidade regiao e vendedor 
+esta se relacionado a um evento 
+consulta principal media venda calculo de media de vendas por região ou vendedor 
+consulta secundaria região ou vendedor se nao tiver um vai outro  
