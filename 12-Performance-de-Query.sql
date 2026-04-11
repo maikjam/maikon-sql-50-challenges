21fda871b44b0a75ba7b36952ced023be6bdfd5a
maikjam <maikonlucas3135@outlook.com>
2026-04-10 23:43:59 -0300

    Performance de Query
    Performance de Query

---
+++ 12-Performance-de-Query.sql
@@ -0,0 +1,9 @@
+/*
+Desafio 10: Performance de Query - Seek vs Scan (Monitoramento)
+Foco: Otimização e Indexação.
+
+Tabelas: Sales.SalesOrderDetail (sod).
+Tarefa: Comparar o plano de execução ao filtrar por CarrierTrackingNumber vs SalesOrderID.
+Nota Técnica: Analisar a diferença de tempo e custo de CPU no Execution Plan do SSMS.
+Objetivo: Diferenciar o perfil de Arquiteto de Dados na entrega de sistemas de baixa latência.
+*/
